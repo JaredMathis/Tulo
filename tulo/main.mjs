4665f9fea7dd7a3d7313c76157588a7723023ac1
JaredMathis <jared.mathis@native.consulting>
2022-04-07 11:53:58 -0400

    progress

--- tulo/main.mjs
+++ tulo/main.mjs
@@ -1,7 +1,8 @@
 import element_add from '../element/add.mjs'
 import element_html_inner from '../element/html_inner.mjs'
+import t from '../english/top100.txt.json' assert { type: 'json' };
 
 export default function tulo_main(parent) {
     let div = element_add(parent, 'div');
-    element_html_inner(div, 'hello world')
+    element_html_inner(div, 'hello world ' + t)
 }
