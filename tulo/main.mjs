19c850812ce7f0ae369447b9633334067bb90ded
JaredMathis <jared.mathis@native.consulting>
2022-04-07 17:51:08 -0400

    play new word audio

--- tulo/main.mjs
+++ tulo/main.mjs
@@ -64,6 +64,7 @@ export default function tulo_main(parent) {
             let container_rosetta = element_add(parent, 'div')
             element_html_inner(element_add(container_rosetta, 'div'), 'New word: ');
             let rosetta = component_rosetta(container_rosetta, _.last(words));
+            rosetta.translated.play();
             element_on_click(element_button_primary(container_rosetta, 'Okay!'), () => {
                 element_hide(container_rosetta)
                 element_show(container)
