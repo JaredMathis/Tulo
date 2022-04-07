37a32f10b9e2a75453fa44fc9cb86654682f68bb
JaredMathis <jared.mathis@native.consulting>
2022-04-07 13:41:37 -0400

    progress

--- tulo/main.mjs
+++ tulo/main.mjs
@@ -13,51 +13,59 @@ function js_random_integer(max) {
 
 export default function tulo_main(parent) {
     let first_4 = top100.slice(0, 4);
-
     let choices_english = first_4;
 
-    let question_english = choices_english[js_random_integer(choices_english.length)]
+    refresh();
 
-    let yes = Math.random() > 0.5;
+    function refresh() {
+        // clear
+        element_html_inner(parent, '');
 
-    let prompt_english;
-    if (yes) {
-        prompt_english = question_english;
-    } else {
-        let wrong_choices_english = _.without(choices_english, question_english);
-        // console.log({wrong_choices_english})
-        let prompt_index = js_random_integer(wrong_choices_english.length)
-        prompt_english = wrong_choices_english[prompt_index]
+        let container = element_add(parent, 'div');
+            
+        let question_english = choices_english[js_random_integer(choices_english.length)]
+
+        let yes = Math.random() > 0.5;
+
+        let prompt_english;
+        if (yes) {
+            prompt_english = question_english;
+        } else {
+            let wrong_choices_english = _.without(choices_english, question_english);
+            // console.log({wrong_choices_english})
+            let prompt_index = js_random_integer(wrong_choices_english.length)
+            prompt_english = wrong_choices_english[prompt_index]
+        }
+
+        phrase_untranslated(element_add(container, 'div'), question_english);    
+
+        phrase_translated(element_add(container, 'div'), prompt_english);
+
+        let button_yes = element_button_primary(container, 'Yes');
+        element_on_click(button_yes, () => {
+            if (yes) {
+                refresh();
+            } else {
+                button_yes.disabled = true;
+            }
+        })
+
+        let button_no = element_button_primary(container, 'No');
+        element_on_click(button_no, () => {
+            if (yes) {
+                button_no.disabled = true;
+            } else {
+                refresh();
+            }
+        })
+
+        // let header3 = element_add(parent, 'h1');
+        // element_html_inner(header3, yes ? 'Yes' : 'No'); 
+
+        // first_4.forEach(f => {
+        //     element_add(parent, 'hr')
+        //     phrase_untranslated(element_add(parent, 'div'), f)
+        //     phrase_translated(element_add(parent, 'div'), f)
+        // })
     }
-
-    phrase_untranslated(element_add(parent, 'div'), question_english);    
-
-    phrase_translated(element_add(parent, 'div'), prompt_english);
-
-    let button_yes = element_button_primary(parent, 'Yes');
-    element_on_click(button_yes, () => {
-        if (yes) {
-            alert('correct')
-        } else {
-            button_yes.disabled = true;
-        }
-    })
-
-    let button_no = element_button_primary(parent, 'No');
-    element_on_click(button_no, () => {
-        if (yes) {
-            button_no.disabled = true;
-        } else {
-            alert('correct')
-        }
-    })
-
-    // let header3 = element_add(parent, 'h1');
-    // element_html_inner(header3, yes ? 'Yes' : 'No'); 
-
-    // first_4.forEach(f => {
-    //     element_add(parent, 'hr')
-    //     phrase_untranslated(element_add(parent, 'div'), f)
-    //     phrase_translated(element_add(parent, 'div'), f)
-    // })
 }
