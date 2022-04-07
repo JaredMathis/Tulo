276da6b23237fdf6b81052342ed7918064b091f2
JaredMathis <jared.mathis@native.consulting>
2022-04-07 12:32:06 -0400

    progress

---
+++ element/on_click.mjs
@@ -0,0 +1,3 @@
+export default function element_on_click(element, on_click) {
+    element.addEventListener("click", on_click);
+}
--- tulo/main.mjs
+++ tulo/main.mjs
@@ -1,12 +1,21 @@
 import element_add from '../element/add.mjs'
 import element_html_inner from '../element/html_inner.mjs'
+import element_on_click from '../element/on_click.mjs'
 import top100 from '../english/top100.txt.json' assert { type: 'json' };
+import translations from '../languages/ceb/translations.json' assert { type: 'json' };
 
 export default function tulo_main(parent) {
     let first_4 = top100.slice(0, 4);
 
     first_4.forEach(f => {
         let element = element_add(parent, 'h1');
-        element_html_inner(element, f)
+        let translated = translations[f];
+        element_html_inner(element, f + ' ' + translated)
+
+        element_on_click(element, () => {
+            
+            var audio = new Audio(`.\\languages\\fil-PH\\audio\\${translated}.mp3`);
+            audio.play();
+        })
     })
 }
