aa380dad64a178848137fde27e3bcf8259894fd7
JaredMathis <jared.mathis@native.consulting>
2022-04-07 11:16:36 -0400

    refactor

--- speech.mjs
+++ speech.mjs
@@ -1,45 +1,9 @@
-import textToSpeech from '@google-cloud/text-to-speech';
-import gcloud_auth_initialize from './gcloud_auth_initialize.mjs';
-import {promises as fs} from 'fs'
 import path from 'path';
-import file_exists from './file_exists.mjs';
-import directory_create_if_not_exists from './directory_create_if_not_exists.mjs';
 import json_read from './json_read.mjs';
-
-gcloud_auth_initialize()
-
-// Creates a client
-const client = new textToSpeech.TextToSpeechClient();
+import gcloud_text_to_speech from './gcloud_text_to_speech.mjs';
 
 // The text to synthesize
 const languageCode = 'fil-PH';
-
-async function gcloud_text_to_speech(text, languageCode) {
-  let audio_directory = path.join('.', 'languages', languageCode, 'audio');
-  await directory_create_if_not_exists(audio_directory);
-
-  let output_path = path.join(audio_directory, text + '.mp3')
-  if (await file_exists(output_path)) {
-    console.log('skipping ' + output_path);
-    return;
-  }
-
-  // Construct the request
-  const request = {
-    input: {text: text},
-    // Select the language and SSML voice gender (optional)
-    voice: {languageCode: languageCode, ssmlGender: 'NEUTRAL'},
-    // select the type of audio encoding
-    audioConfig: {audioEncoding: 'MP3'},
-  };
-
-  // Performs the text-to-speech request
-  const [response] = await client.synthesizeSpeech(request);
-  // Write the binary audio content to a local file
-  const writeFile = fs.writeFile;
-  await writeFile(output_path, response.audioContent, 'binary');
-  console.log('Audio content written to file: ' + output_path);
-}
 
 const targetLanguageCode = 'ceb';
 
