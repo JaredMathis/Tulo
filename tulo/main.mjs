60ec2b8cd72d0346153484955f3c6ad9db3aa501
JaredMathis <jared.mathis@native.consulting>
2022-04-08 09:30:32 -0400

    changes

--- tulo/main.mjs
+++ tulo/main.mjs
@@ -16,20 +16,21 @@ function js_random_integer(max) {
 }
 
 export default function tulo_main(parent) {
-    let word_count = 16;
+    let word_count = 18;
     let round_count_max = 12;
-    let round_count = round_count_max;
-    let skipped_words = [
-        'a',
-    ]
+    let round_count = 0;
+    let skip_word_if = word => {
+        return translations[word][0].toLowerCase() === word.toLowerCase()
+    }
     let choice_count = 4;
     let answers_from_previous = 4;
     let words = words_get();
+    let is_review = true;
 
     element_on_click(element_button_primary(parent, 'Start'), refresh);
 
     function words_get() {
-        let filtered = top100.filter(w => !skipped_words.includes(w));
+        let filtered = top100.filter(w => !skip_word_if(w));
         let words_sorted = filtered.slice(0, word_count);
         let words = words_sorted;
         return words;
@@ -40,7 +41,7 @@ export default function tulo_main(parent) {
         element_html_inner(parent, '');
 
         let round_new = false;
-        if (round_count === round_count_max) {
+        if (!is_review && round_count === round_count_max) {
             round_count = 1;
             word_count++;
             words = words_get();
@@ -53,13 +54,13 @@ export default function tulo_main(parent) {
 
         let result;
         if (js_random_integer(2) === 0) {
-            result = refresh_multiple_translated_to_untranslated();
+            result = refresh_multiple_translated_to_untranslated(is_review);
         } else {
-            result = refresh_multiple_untranslated_to_translated();
+            result = refresh_multiple_untranslated_to_translated(is_review);
         }
         let { container } = result;
 
-        if (round_new) {
+        if (!is_review && round_new) {
             element_hide(container)
 
             let container_rosetta = element_add(parent, 'div')
@@ -93,12 +94,18 @@ export default function tulo_main(parent) {
             }
         }
 
-        function refresh_multiple_generic(on_success, question_phrase, choice_phrase) {
+        function refresh_multiple_generic(
+            is_review, on_success, question_phrase, choice_phrase) {
             let container = element_add(parent, 'div');
 
             // These are the words we're working on right now
-            let answers = words.slice(word_count - answers_from_previous, word_count);
-            let answer = answers[js_random_integer(answers_from_previous)];
+            let answers;
+            if (is_review) {
+                answers = words;
+            } else {
+                answers = words.slice(word_count - answers_from_previous, word_count);
+            }
+            let answer = answers[js_random_integer(answers.length)];
 
             let words_without_answer = _.without(words, answer);
             words_without_answer = _.shuffle(words_without_answer);
@@ -148,12 +155,13 @@ export default function tulo_main(parent) {
         }
 
         
-        function refresh_multiple_translated_to_untranslated() {
+        function refresh_multiple_translated_to_untranslated(is_review) {
             let result;
             let onload = () => {
                 result.element_question.play();
             }
             result = refresh_multiple_generic(
+                is_review,
                 (choice_element, question_element) => {
                     question_element.audio.addEventListener('ended', () => {
                         refresh();
@@ -167,9 +175,10 @@ export default function tulo_main(parent) {
             return result;
         }
 
-        function refresh_multiple_untranslated_to_translated() {
+        function refresh_multiple_untranslated_to_translated(is_review) {
             let onload = _.noop
             let result = refresh_multiple_generic(
+                is_review,
                 (choice_element) => {
                     choice_element.audio.addEventListener('ended', () => {
                         refresh();
