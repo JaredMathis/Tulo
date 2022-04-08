763371ce57dbf42cb85a75dcff42c1a0553f33ed
JaredMathis <jared.mathis@native.consulting>
2022-04-08 16:47:55 -0400

    refactor

--- tulo/main.mjs
+++ tulo/main.mjs
@@ -110,10 +110,9 @@ export default function tulo_main(parent) {
 
         if (mode === mode_practice_mistakes) {
             element_html_inner(element_add(container_labels, 'div'), 'You have ' + mistakes.length + ' mistake(s) to practice.')
-        } else {
-            if (mode === mode_learn_new) {
-                element_html_inner(element_add(container_labels, 'div'), 'You will learn a new word after question ' + question_count_max + ". ")
-            }
+        }
+        if (mode === mode_learn_new) {
+            element_html_inner(element_add(container_labels, 'div'), 'You will learn a new word after question ' + question_count_max + ". ")
         }
 
         element_html_inner(element_add(container_labels, 'div'), 'You have learned ' + word_count + ' word(s).')
