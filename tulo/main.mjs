f91a4c00498156d90ec928d2912e04465f62e77c
JaredMathis <jared.mathis@native.consulting>
2022-04-08 09:43:54 -0400

    refactor

--- tulo/main.mjs
+++ tulo/main.mjs
@@ -50,7 +50,8 @@ export default function tulo_main(parent) {
             round_count++;
         }
 
-        element_html_inner(element_add(parent, 'div'), 'Round: ' + round_count + ' / ' + round_count_max + '; words: ' + word_count)
+        let label = 'Round: ' + round_count + ' / ' + round_count_max + '; words: ' + word_count;
+        element_html_inner(element_add(parent, 'div'), label)
 
         // These are the words we're working on right now
         let answers;
