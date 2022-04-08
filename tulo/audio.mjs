8327d170e763e7cdf4b417b71b67a5feec47ad80
JaredMathis <jared.mathis@native.consulting>
2022-04-08 19:42:09 -0400

    fix file path for audio

--- tulo/audio.mjs
+++ tulo/audio.mjs
@@ -1,4 +1,6 @@
+import string_to_file_path from "../js/string_to_file_path.mjs"
+
 export default function tulo_audio(translated) {
-    var audio = new Audio(`.\\languages\\fil-PH\\audio\\${translated}.mp3`);
+    var audio = new Audio(`.\\languages\\fil-PH\\audio\\${string_to_file_path(translated)}.mp3`);
     return audio;
 }
