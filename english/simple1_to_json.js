5e241eaba307bcfb4ec686f7bff7d87a3fb7e139
JaredMathis <jared.mathis@native.consulting>
2022-04-08 18:56:46 -0400

    progress

---
+++ english/simple1_to_json.js
@@ -0,0 +1,7 @@
+import {promises as fs} from 'fs'
+
+let path_top100 = './english/simple1.txt';
+let top100_text = await fs.readFile(path_top100, 'utf8')
+let rows = top100_text.split('\n');
+let mapped = rows.map(row => row.split('\t')[0].trim())
+await fs.writeFile(path_top100 + '.json', JSON.stringify(mapped, null, 2));
