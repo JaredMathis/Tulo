6d6a43fef75d05d0a514e6356640098f613d26f6
JaredMathis <jared.mathis@native.consulting>
2022-04-08 21:19:19 -0400

    rename

--- gcloud/speech.mjs
+++ gcloud/speech.mjs
@@ -6,12 +6,12 @@ const languageCode = 'fil-PH';
 
 const targetLanguageCode = 'ceb';
 
-let translations_directory = './languages/' + targetLanguageCode;
-let path_translations = path.join(translations_directory, 'translations.json');
-let translations = await json_read(path_translations);
+let translations_directory = './translations/';
+let path_translations = path.join(translations_directory, `en_${targetLanguageCode}.json`);
+let to = await json_read(path_translations);
 
-for (let english in translations) {
-  for (let translated of translations[english]) {
+for (let english in to) {
+  for (let translated of to[english]) {
     await gcloud_text_to_speech(translated, languageCode);
   }
 }
