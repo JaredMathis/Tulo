4e1ccd0973dbcb05359c22a6c2dc06d377d687df
JaredMathis <jared.mathis@native.consulting>
2022-04-08 21:58:04 -0400

    composite

---
+++ translations/composite.js
@@ -0,0 +1,23 @@
+import {promises as fs} from 'fs'
+import _ from 'lodash'
+
+let en_ceb = JSON.parse(await fs.readFile('./translations/en_ceb.json'));
+let ceb_en = JSON.parse(await fs.readFile('./translations/ceb_en.json'));
+
+for (let ceb in ceb_en) {
+    for (let en of ceb_en[ceb]) {
+        if (!en_ceb[en]) {
+            en_ceb[en] = [];
+        }
+        if (!en_ceb[en].includes(ceb)) {
+            en_ceb[en].push(ceb)
+        }
+    }
+}
+
+for (let en in en_ceb) {
+    let v = _.uniq(en_ceb[en].map(ceb => ceb.toLowerCase()));
+    en_ceb[en] = v;
+}
+
+await fs.writeFile('./translations/en_ceb_composite.json', JSON.stringify(en_ceb, null, 2));
