fc0d1f96fd40ef1cfaf2f14d1cf2b2244388d99d
JaredMathis <jared.mathis@native.consulting>
2022-04-08 21:19:57 -0400

    refactor

--- gcloud/speech.mjs
+++ gcloud/speech.mjs
@@ -2,13 +2,16 @@ import path from 'path';
 import json_read from '../js/json_read.mjs';
 import gcloud_text_to_speech from './text_to_speech.mjs';
 
-const languageCode = 'fil-PH';
-
 const targetLanguageCode = 'ceb';
 
 let translations_directory = './translations/';
 let path_translations = path.join(translations_directory, `en_${targetLanguageCode}.json`);
 let to = await json_read(path_translations);
+
+path_translations = path.join(translations_directory, `${targetLanguageCode}_en.json`);
+let from = await json_read(path_translations);
+
+const languageCode = 'fil-PH';
 
 for (let english in to) {
   for (let translated of to[english]) {
