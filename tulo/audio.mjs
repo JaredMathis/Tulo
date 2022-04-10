c822e9f9cef2e1fc1dba7443249c485a83027133
JaredMathis <jared.mathis@native.consulting>
2022-04-10 19:34:59 -0400

    progress

--- tulo/audio.mjs
+++ tulo/audio.mjs
@@ -1,6 +1,6 @@
 import string_to_file_path from "../js/string_to_file_path.mjs"
 
 export default function tulo_audio(translated) {
-    var audio = new Audio(`.\\audio\\fil-PH\\${string_to_file_path(translated)}.mp3`);
+    var audio = new Audio(`..\\Tulo\\audio\\fil-PH\\${string_to_file_path(translated)}.mp3`);
     return audio;
 }
