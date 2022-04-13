7a9e9f507959237b4a55f42b251a176e6de2e76d
JaredMathis <jared.mathis@native.consulting>
2022-04-12 20:07:39 -0400

    remove duplicates

--- words/james_1_to_en.mjs
+++ words/james_1_to_en.mjs
@@ -1,12 +1,14 @@
 import {promises as fs} from 'fs'
 import tulo_translate_reverse from '../tulo/translate_reverse.mjs';
+import _ from 'lodash'
 
 let path_james_1 = './words/james_1.json';
 let james_1_text = await fs.readFile(path_james_1, 'utf8')
 let james_1 = JSON.parse(james_1_text);
 
 let path_james_1_en = './words/james_1.en.json';
-let james_1_en = james_1.map(w => tulo_translate_reverse(w))
+let james_1_en_duplicates = james_1.map(w => tulo_translate_reverse(w))
+let james_1_en = _.uniq(james_1_en_duplicates);
 let james_1_en_text = JSON.stringify(james_1_en, null, 2);
 await fs.writeFile(path_james_1_en, james_1_en_text)
 
